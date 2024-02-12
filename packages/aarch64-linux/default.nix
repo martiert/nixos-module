4c2b5edbd7503284e0cb244484f32ce4748d5d6f
Martin Ertsås <martiert@gmail.com>
2024-02-12 21:08:36 +0100

    packages/aarch64-linux: Add the sc8280xp kernel

--- packages/aarch64-linux/default.nix
+++ packages/aarch64-linux/default.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 
+let
+  sc8280xpKernel = pkgs.callPackage ../../machines/sc8280xp/kernel {};
+in
 rec {
   linux-firmware-x13s = pkgs.callPackage ./x13s-firmware {};
   qrtr = pkgs.callPackage ./qrtr {};
   pd-mapper = pkgs.callPackage ./pd-mapper { inherit qrtr; };
+  sc8280xp_kernel = sc8280xpKernel.kernel;
 }
