27503dafdf473018c5c0280acc07e42f1420932e
Martin Ertsås <martiert@gmail.com>
2024-02-12 20:54:39 +0100

    sc8280xp: Update to kernel 6.6.12

--- machines/sc8280xp/kernel/default.nix
+++ machines/sc8280xp/kernel/default.nix
@@ -3,17 +3,17 @@
 let
   steev_kernel_pkg = { buildLinux, ... }@args:
    buildLinux (args // rec {
-     version = "6.6.11";
+     version = "6.6.12";
      modDirVersion = version;
-     # defconfig = ./defconfig;
      defconfig = "laptop_defconfig";
 
      src = pkgs.fetchFromGitHub {
        owner = "steev";
        repo = "linux";
-       rev = "48e40671c262d6b4ed199290d6c8d8631f8f7970";
-       hash = "sha256-vvT92Y7KVhrdqmGOxWoYTixBSMzzgvZ4U81v/Uz2FtI=";
+       rev = "0d6a5d6312e4ad69b18d699c48ee93a9ba564dfb";
+       hash = "sha256-dyItbyfVmcwJw/i/KYHRMrXbO6JSmQEckxFD66jRBm0=";
      };
+
      kernelPatches = [
        {
          name = "Add firmware";
