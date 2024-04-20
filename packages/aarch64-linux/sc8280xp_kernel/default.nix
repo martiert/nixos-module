e2ee6f9853d50fcb4ac8cf47627d5333b993e3b4
Martin Ertsås <martiert@gmail.com>
2024-04-20 12:30:43 +0200

    Revert "sc8280xp-kernel: Update to 6.8.5"
    This reverts commit deb37747830c8a17f08720c0858d175fc921bc78.

--- packages/aarch64-linux/sc8280xp_kernel/default.nix
+++ packages/aarch64-linux/sc8280xp_kernel/default.nix
@@ -3,15 +3,15 @@
 let
   kernel_pkg = { buildLinux, ... }@args:
    buildLinux (args // rec {
-     version = "6.8.5";
+     version = "6.7.5";
      modDirVersion = version;
      defconfig = "laptop_defconfig";
 
      src = pkgs.fetchFromGitHub {
        owner = "steev";
        repo = "linux";
-       rev = "58a46f49aaf7d978d8a8883137a41ad21f8e8f84";
-       hash = "sha256-HQeDMvTNEo8/SaioZhlkuzcDwfbqFhaJR/5d4y2dvgg=";
+       rev = "45411854bb931cb5c989e00ded127d4fc68dfa01";
+       hash = "sha256-2FSsuKEl+KTYmiDtgCbxHS7y8Qz+Y1zkRuRcWWE0iqc=";
      };
 
      kernelPatches = [
