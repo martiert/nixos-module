714dde4a99a73227402e28403b12ab50ab91542b
Martin Ertsås <martiert@gmail.com>
2024-05-13 10:05:42 +0200

    sc8280xp_kernel: Update to 6.8.9

--- packages/aarch64-linux/sc8280xp_kernel/default.nix
+++ packages/aarch64-linux/sc8280xp_kernel/default.nix
@@ -3,15 +3,15 @@
 let
   kernel_pkg = { buildLinux, ... }@args:
    buildLinux (args // rec {
-     version = "6.7.5";
+     version = "6.8.9";
      modDirVersion = version;
      defconfig = "laptop_defconfig";
 
      src = pkgs.fetchFromGitHub {
        owner = "steev";
        repo = "linux";
-       rev = "45411854bb931cb5c989e00ded127d4fc68dfa01";
-       hash = "sha256-2FSsuKEl+KTYmiDtgCbxHS7y8Qz+Y1zkRuRcWWE0iqc=";
+       rev = "d6c0993fc013b17e0b1bbed172554b0f1cba0bfd";
+       hash = "sha256-Z35n8rah4z1eL4+JvlI//cpH2TzXYTDf7r0SvrTGDcE=";
      };
 
      kernelPatches = [
