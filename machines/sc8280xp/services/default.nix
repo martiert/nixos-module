839342c342c1ecc1697420007c0203c917ae3827
Martin Ertsås <martiert@gmail.com>
2024-06-05 09:32:33 +0200

    sc8280xp: Remove pd-mapper
    It is now a part of the kernel

--- machines/sc8280xp/services/default.nix
+++ machines/sc8280xp/services/default.nix
@@ -1,14 +1,6 @@
 { pkgs, lib, config, ... }:
 
 lib.mkIf (config.martiert.system.aarch64.arch == "sc8280xp") {
-  nixpkgs = {
-    overlays = [
-      (final: prev: {
-        compressFirmwareXz = lib.id; # pd-mapper needs firmware to not be compressed
-      })
-    ];
-  };
-
   hardware.pulseaudio.enable = false;
 
   services.upower.enable = true;
@@ -17,25 +9,6 @@ lib.mkIf (config.martiert.system.aarch64.arch == "sc8280xp") {
   ];
 
   systemd.services = {
-    pd-mapper = {
-      unitConfig = {
-        Requires = "qrtr.service";
-        After = "qrtr.service";
-      };
-      serviceConfig = {
-        ExecStart = "${pkgs.pd-mapper}/bin/pd-mapper";
-        Restart = "always";
-      };
-      wantedBy = ["multi-user.target"];
-      before = ["upower.service"];
-    };
-    qrtr = {
-      serviceConfig = {
-        ExecStart = "${pkgs.qrtr}/bin/qrtr-ns -f 1";
-        Restart = "always";
-      };
-      wantedBy = ["multi-user.target"];
-    };
     bluetooth = {
       serviceConfig = {
         ExecStartPre = [
--- packages/aarch64-linux/default.nix
+++ packages/aarch64-linux/default.nix
@@ -5,7 +5,5 @@ let
 in
 rec {
   linux-firmware-x13s = pkgs.callPackage ./x13s-firmware {};
-  qrtr = pkgs.callPackage ./qrtr {};
-  pd-mapper = pkgs.callPackage ./pd-mapper { inherit qrtr; };
   sc8280xp_kernel = pkgs.callPackage ./sc8280xp_kernel {};
 }
