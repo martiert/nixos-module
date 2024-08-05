107570f6fe38d9d36997686bf872edde6667d8a8
Martin Ertsås <mertsas@cisco.com>
2024-08-05 09:57:37 +0200

    sc8280xp kernel: Update to 6.10.2

--- packages/aarch64-linux/sc8280xp_kernel/default.nix
+++ packages/aarch64-linux/sc8280xp_kernel/default.nix
@@ -3,15 +3,15 @@
 let
   kernel_pkg = { buildLinux, ... }@args:
    buildLinux (args // rec {
-     version = "6.9.2";
+     version = "6.10.2";
      modDirVersion = version;
      defconfig = "defconfig";
 
      src = pkgs.fetchFromGitHub {
        owner = "steev";
        repo = "linux";
-       rev = "4ad79d624a0a64db0b59f60b4c2225c8a5b92f96";
-       hash = "sha256-2lgDyPOFjGj143CSbSHL5cqJhQIQj0Grbcpno1JVhGo=";
+       rev = "03629588630c1f5736b5c398f9c5d851bf6b8e7a";
+       hash = "sha256-IEQ8bg6KLReR9qY3AQjZqf+AtIaLnYWv5cUtAMNMeos=";
      };
 
      kernelPatches = [
