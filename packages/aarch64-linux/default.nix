577e5a7c6937d6e937bdf475d8d89d161667b520
Martin Ertsås <martiert@gmail.com>
2024-09-03 10:56:45 +0200

    Add a pinebook kernel

--- packages/aarch64-linux/default.nix
+++ packages/aarch64-linux/default.nix
@@ -6,4 +6,5 @@ in
 rec {
   linux-firmware-x13s = pkgs.callPackage ./x13s-firmware {};
   sc8280xp_kernel = pkgs.callPackage ./sc8280xp_kernel {};
+  pinebook_kernel = pkgs.callPackage ./pinebook_kernel {};
 } // pkgs.callPackages ./rpi_kernels {}
--- packages/aarch64-linux/sc8280xp_kernel/default.nix
+++ packages/aarch64-linux/sc8280xp_kernel/default.nix
@@ -3,15 +3,15 @@
 let
   kernel_pkg = { buildLinux, ... }@args:
    buildLinux (args // rec {
-     version = "6.10.2";
+     version = "6.10.6";
      modDirVersion = version;
      defconfig = "defconfig";
 
      src = pkgs.fetchFromGitHub {
        owner = "steev";
        repo = "linux";
-       rev = "03629588630c1f5736b5c398f9c5d851bf6b8e7a";
-       hash = "sha256-IEQ8bg6KLReR9qY3AQjZqf+AtIaLnYWv5cUtAMNMeos=";
+       rev = "dcc28592778a6a65f462becf0bc57813810f7dde";
+       hash = "sha256-AYBUw5tTklVJZOlC/aKEeIJo4Qz2TtFiYPS2bzM9tDs=";
      };
 
      kernelPatches = [
