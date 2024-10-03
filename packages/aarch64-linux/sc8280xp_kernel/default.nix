b85b963ef5ea9413599ff4e75a4aab70c91669da
Martin Ertsås <martiert@gmail.com>
2024-10-03 08:26:10 +0200

    sc8280xp_kernel: Update to kernel 6.11.0

--- packages/aarch64-linux/sc8280xp_kernel/default.nix
+++ packages/aarch64-linux/sc8280xp_kernel/default.nix
@@ -3,15 +3,15 @@
 let
   kernel_pkg = { buildLinux, ... }@args:
    buildLinux (args // rec {
-     version = "6.10.6";
+     version = "6.11.0";
      modDirVersion = version;
      defconfig = "defconfig";
 
      src = pkgs.fetchFromGitHub {
        owner = "steev";
        repo = "linux";
-       rev = "dcc28592778a6a65f462becf0bc57813810f7dde";
-       hash = "sha256-AYBUw5tTklVJZOlC/aKEeIJo4Qz2TtFiYPS2bzM9tDs=";
+       rev = "c096e56788fa6227120b84d46d39ffd522fb1ede";
+       hash = "sha256-NPtlV3oJ8+Xx5MIYg1tap871iqxR659eKjfT0o78ETM=";
      };
 
      kernelPatches = [
