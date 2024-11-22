987173e42f81834a24cf1d9c91b398aa698d50ed
Martin Ertsås <mertsas@cisco.com>
2024-11-22 08:29:56 +0100

    Remove pinebook and rpi kernels

--- packages/aarch64-linux/default.nix
+++ packages/aarch64-linux/default.nix
@@ -6,5 +6,4 @@ in
 rec {
   linux-firmware-x13s = pkgs.callPackage ./x13s-firmware {};
   sc8280xp_kernel = pkgs.callPackage ./sc8280xp_kernel {};
-  pinebook_kernel = pkgs.callPackage ./pinebook_kernel {};
-} // pkgs.callPackages ./rpi_kernels {}
+}
--- packages/aarch64-linux/rpi_kernels/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-{
-  linux_rpi5 = pkgs.linux_rpi4.override {
-    rpiVersion = 5;
-    argsOverride.defconfig = "bcm2712_defconfig";
-  };
-}
