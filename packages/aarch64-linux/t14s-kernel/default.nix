884eb8819834a6a0bcce19f297e483bc6691c2ea
Martin Ertsås <martiert@gmail.com>
2025-09-08 08:41:20 +0200

    Make the t14s kernel just a kernel

--- packages/aarch64-linux/t14s-kernel/default.nix
+++ packages/aarch64-linux/t14s-kernel/default.nix
@@ -1,6 +1,6 @@
-{ linuxPackagesFor, linuxPackages_latest, lib, ... }:
+{ linuxPackages_latest, lib, ... }:
 
-linuxPackagesFor (linuxPackages_latest.kernel.override {
+linuxPackages_latest.kernel.override {
   structuredExtraConfig = with lib.kernel; {
     TYPEC = yes;
     PHY_QCOM_QMP = yes;
@@ -10,4 +10,4 @@ linuxPackagesFor (linuxPackages_latest.kernel.override {
     PHY_QCOM_QMP_PCIE = yes;
     CLK_X1E80100_CAMCC = yes;
   };
-})
+}
