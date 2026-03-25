5d2a7ce79d49e1c179625ed76536277d7db68de9
Martin Ertsås <martiert@gmail.com>
2026-03-25 13:47:28 +0100

    t14s-kernel: Add more configs

--- packages/aarch64-linux/t14s-kernel/default.nix
+++ packages/aarch64-linux/t14s-kernel/default.nix
@@ -2,12 +2,26 @@
 
 linuxPackages_latest.kernel.override {
   structuredExtraConfig = with lib.kernel; {
-    TYPEC = yes;
-    PHY_QCOM_QMP = yes;
-    QCOM_CLK_RPM = yes;
-    MFD_QCOM_RPM = yes;
-    REGULATOR_QCOM_RPM = yes;
-    PHY_QCOM_QMP_PCIE = yes;
     CLK_X1E80100_CAMCC = yes;
+    INTERCONNECT_QCOM = yes;
+
+    MFD_QCOM_RPM = yes;
+    MFD_SPMI_PMIC = yes;
+
+    PHY_QCOM_QMP = yes;
+    PHY_QCOM_QMP_PCIE = yes;
+
+    QCOM_CLK_RPM = yes;
+    QCOM_COMMAND_DB = yes;
+    QCOM_RPMH = yes;
+    QCOM_RPMHPD = yes;
+
+    REGULATOR = yes;
+    REGULATOR_FIXED_VOLTAGE = yes;
+    REGULATOR_QCOM_RPM = yes;
+    REGULATOR_QCOM_RPMH = yes;
+
+    SPMI = yes;
+    TYPEC = yes;
   };
 }
